536419628dbac3b0824b575e59b8afda28f5f542
Daryl Marco <darmarco@gmail.com>
2019-10-10 19:35:58 -0700

    Added API get routing

--- server/index.js
+++ server/index.js
@@ -4,7 +4,27 @@ const bodyParser = require('body-parser');
 const db = require('../database/index.js')
 
 app.use(bodyParser.json());
-app.use(express.static());
+app.use(express.static(public));
+
+app.get('/api-reviews', (req, res) => {
+  db.getReviewsByProductId(req.body, (err, results) => {
+    if (err) {
+      console.log err;
+    } else {
+      res.end(results);
+    }
+  })
+})
+
+app.get('/api-product-data', (req, res) => {
+  db.getProductDataById(req.body, (err, results) => {
+    if (err) {
+      console.log err;
+    } else {
+      res.end(results);
+    }
+  })
+})
 
 app.listen(3003, function(){
   console.log(`Review server listening on port $(port), ${new Date()}`);
