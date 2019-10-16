f9ddf133f0a7d4e094a2c6e0d6bb6c1ef4e64f62
Daryl Marco <darmarco@gmail.com>
2019-10-16 14:36:29 -0700

    Server setup for helpful / report review incrementing counts

--- database/index.js
+++ database/index.js
@@ -27,5 +27,14 @@ var getReviewsByProductId = function(req, callback) {
   })
 }
 
+var incrementReviewCounts = function(req, callback) {
+  connection.query("UPDATE reviews SET ? = ? + 1 WHERE id = ?", req.column, req.column, req.id, (error, results, fields) => {
+    if (error) {
+      console.log(error);
+    }
+    callback(null, results);
+  })
+}
+
 module.exports.getProductDataById = getProductDataById;
 module.exports.getReviewsByProductId = getReviewsByProductId;
