3fbe720fefe17f6e19acb4702148407b5212e671
“silkyh13” <silkyh13@gmail.com>
2019-10-31 08:08:34 -0400

    fixed connection.escape()

--- database/index.js
+++ database/index.js
@@ -13,10 +13,11 @@ connection.connect();
 // not touching this
 const getProductDataById = function (req, callback) {
   connection.query('SELECT * FROM product_data WHERE id = ?', req.product_id, (error, results, fields) => {
-    if (error) {
-      return error;
+    try {
+      callback(null, results);
+    } catch (e) {
+      console.error(e);
     }
-    callback(null, results);
   });
 };
 
@@ -25,44 +26,48 @@ const postReviewsByProductId = function (product, callback) {
   const queryString = 'Insert into reviews (product_id, title, text, date, author, overall_rating, value_rating, quality_rating, appearance_rating, ease_of_assembly_rating, works_as_expected_rating, recommended, helpful_count, not_helpful_count) VALUES(?, ?, ?, ?,?, ?, ?, ?,?, ?, ?, ?,?, ?)';
 
   connection.query(queryString, (Object.values(product)), (error, results) => {
-    if (error) {
-      return error;
+    try {
+      callback(null, results);
+    } catch (e) {
+      console.error(e);
     }
-    callback(null, results);
   });
 };
 
 
 // get reviews for one product
 const getReviewsByProductId = function (productid, callback) {
-  const queryString = `SELECT * FROM reviews WHERE product_id = ${connection.escape(productid)}`;
+  const queryString = `SELECT * FROM reviews WHERE product_id = ${productid}`;
   connection.query(queryString, (error, results) => {
-    if (error) {
-      return error;
+    try {
+      callback(null, results);
+    } catch (e) {
+      console.error(e);
     }
-    callback(null, results);
   });
 };
 
 // update
 const incrementReviewCounts = function (req, callback) {
   connection.query('UPDATE reviews SET ?? = ?? + 1 WHERE id = ?', [req.column, req.column, req.id], (error, results, fields) => {
-    if (error) {
-      return error;
+    try {
+      callback(null, results);
+    } catch (e) {
+      console.error(e);
     }
-    callback(null, results);
   });
 };
 
 
 // delete one review instead all review for one product
 const deleteReviewsById = function (id, callback) {
-  const queryString = `delete from reviews where id =${connection.escape(id)}`;
+  const queryString = `delete from reviews where id =${id}`;
   connection.query(queryString, (error, results) => {
-    if (error) {
-      throw error;
+    try {
+      callback(null, results[0]);
+    } catch (e) {
+      console.error(e);
     }
-    callback(null, results[0]);
   });
 };
 
