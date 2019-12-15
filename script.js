44a9ad46c2de603d6ff47757ad0d2d620f1e4b64
“silkyh13” <silkyh13@gmail.com>
2019-12-15 06:41:39 -0500

    refactored stress test

--- script.js
+++ script.js
@@ -2,26 +2,33 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export const options = {
-  vus: 10,
-  duration: '30s',
-  rps: 1000,
-  throw: true,
+  // vus: 10,
+  // duration: '120s',
+  // rps: 1000,
+  // throw: true,
+  stages: [
+    { duration: '30s', rps: 1 },
+    { duration: '30s', rps: 10 },
+    { duration: '30s', rps: 100 },
+    { duration: '30s', rps: 1000 },
+  ],
 };
 
 export default function () {
+  // static
   const res1 = http.get(`http://localhost:3003/?${Math.round(Math.random() * 10000000)}`);
 
-  const res2 = http.get(`http://localhost:3003/api-reviews/${Math.round(Math.random() * 120000000)}`);
+  // const res2 = http.get(`http://localhost:3003/${Math.round(Math.random() * 120000000)}`);
 
-  const res3 = http.get(`http://localhost:3003/api-product-data/?id=${Math.round(Math.random() * 10000000)}`);
+  const res3 = http.get(`http://localhost:3003/api-reviews/${Math.round(Math.random() * 10000000)}`);
   check(res1, {
     'status was 200': (r) => r.status == 200,
     'transaction time OK': (r) => r.timings.duration < 200,
   });
-  check(res2, {
-    'status was 200': (r) => r.status == 200,
-    'transaction time OK': (r) => r.timings.duration < 200,
-  });
+  // check(res2, {
+  //   'status was 200': (r) => r.status == 200,
+  //   'transaction time OK': (r) => r.timings.duration < 200,
+  // });
   check(res3, {
     'status was 200': (r) => r.status == 200,
     'transaction time OK': (r) => r.timings.duration < 200,
